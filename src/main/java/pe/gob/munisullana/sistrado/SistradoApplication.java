7d325077761cff87323b032a09cb9fe5f8baf517
Hugo Ángeles <hugo.angeles.chavez@ibm.com>
2021-01-07 22:05:48 -0500

    Initial commit

---
+++ src/main/java/pe/gob/munisullana/sistrado/SistradoApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.munisullana.sistrado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistradoApplication.class, args);
+	}
+
+}
